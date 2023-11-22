acc92f2456f16e8b305847329f4117ee434c092d
CxWatcher <147360379+CxWatcher@users.noreply.github.com>
2023-11-22 21:32:44 +0000

    Update joelyum.js

--- joelyum.js
+++ joelyum.js
@@ -74,7 +74,7 @@ function retryLoad() {
     .then(response => response.json())
     .then(data => {
       const playback_url = data.playback_url;
-      const src = `https://cors.zimjs.com/${playback_url}`;
+      const src = `${playback_url}`;
       player.src({ type: 'application/x-mpegURL', src });
       player.play();
 	  setTimeout(() => checking = false, 2000);
