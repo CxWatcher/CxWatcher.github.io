195e3f1179ad8bf566bf3c82c87da1840058e371
CxWatcher <147360379+CxWatcher@users.noreply.github.com>
2024-03-09 22:35:03 +0000

    Update custom.js

--- custom.js
+++ custom.js
@@ -15,7 +15,7 @@ overlayBtn.addEventListener('click', function() {
     currentstreamer = userInput;
     retryLoad();
     player.poster('https://i.imgur.com/G2zhTfr.png'); 
-    changeIframeSource(`https://kick.com/${currentstreamer}/chatroom`);
+    changeIframeSource(`https://tools.kick.cx/chat?user=${currentstreamer}&animate=true&badges=true&commands=true&bots=true&textsize=15px`);
   }
 });
 
