76bac114766d5caf8a84ff8f8fe48cbaef1be8bb
CxWatcher <147360379+CxWatcher@users.noreply.github.com>
2024-04-03 15:26:16 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -15,7 +15,7 @@ overlayBtn.addEventListener('click', function() {
     currentstreamer = userInput;
     retryLoad();
     player.poster('https://i.imgur.com/G2zhTfr.png'); 
-    changeIframeSource(`https://cxwatcher.github.io/chat?user=userInput&animate=true&badges=true&commands=true&bots=true&textsize=15px`);
+    changeIframeSource(`https://cxwatcher.github.io/chat?user=${userInput}&animate=true&badges=true&commands=true&bots=true&textsize=15px`);
   }
 });
 
