d7592914909077aa1b6f9e3d6fd55c9e4ae3ec80
CxWatcher <147360379+CxWatcher@users.noreply.github.com>
2024-04-06 03:37:26 +0100

    Update event.js

--- event.js
+++ event.js
@@ -30,7 +30,7 @@ function retryLoad() {
     .then(response => response.json())
     .then(data => {
       const playback_url = data.playback_url;
-      const src = `${playback_url}`;
+      const src = `https://api.codetabs.com/v1/proxy/?quest=${playback_url}`;
       player.src({ type: 'application/x-mpegURL', src });
       player.play();
 	  setTimeout(() => checking = false, 2000);
