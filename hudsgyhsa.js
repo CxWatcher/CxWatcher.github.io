e667acaf312e635ce74d07602eef5aa36d33944e
CxWatcher <147360379+CxWatcher@users.noreply.github.com>
2024-08-15 19:32:09 +0100

    Update hudsgyhsa.js

--- hudsgyhsa.js
+++ hudsgyhsa.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', async function() {
   const iframeCount = 8; 
   const currentStreamers = new Set();
-  const eliminated = new Set(['slykane', '22chainz', 'nedx', 'moises', 'moistcarrots', 'kick_clipz', 'bakedalaska', 'harmoneyes', 'rico-gotti']);
+  const eliminated = new Set(['slykane', '22chainz', 'nedx', 'moises', 'moistcarrots', 'kick_clipz', 'bakedalaska', 'harmoneyes', 'rico-gotti', 'mrfunkefresh', 'bakedalaska', 's0fakinghigh', 'vaultkeeper']);
 
   async function fetchStreamers() {
     try {
